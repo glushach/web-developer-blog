d548be1acff55f3ff79f44e58c621e2109c8bf78
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-07-16 12:19:04 +0300

    feat: use fetch for get data from API cocktails

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,29 +1,56 @@
 import React, { Component } from "react";
 import './stylesNew.css';
-import Fcomponent from "./Fcomponent";
 
+/* use API https://www.thecocktaildb.com/*/
 export default class Ccomponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Button not presses"
+      error: null,
+      isLoaded: false,
+      items: []
     };
-    this.updateData = this.updateData.bind(this);
   }
-  updateData = (value) => {
-    this.setState({
-      name: value
+
+  componentDidMount() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail', {
+      method: 'GET',
     })
+      .then(res => {
+        return res.json()
+      })
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          items: result.drinks
+        });
+      }, (error) => {
+          this.setState({
+            isLoaded: true,
+            error: true
+          });
+        }
+      )
   }
 
   render() {
-   return (
-     <div>
-       {/* pass data from parent to children component*/}
-       <p>State: {this.state.name}</p>
-       <Fcomponent updateData={this.updateData}/>
-     </div>
-   )
+    const {error, isLoaded, items} = this.state;
+    if (error) {
+      return <p>Error {error.message}</p>
+    } else if (!isLoaded) {
+      return <p>Loading...</p>
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.idDrink}>
+              {item.strDrink}
+              <img width={50} src={item.strDrinkThumb}/>
+            </li>
+          ))}
+        </ul>
+      )
+    }
   }
 
 }
